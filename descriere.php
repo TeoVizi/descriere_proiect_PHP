410e99f994e81c109382116dd85eb37688b3e304
Vizinteanu Teodora <115303402+TeoVizi@users.noreply.github.com>
2023-11-03 17:54:38 +0200

    testing css and php together

---
+++ descriere.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Descriere proiect PHP</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+
+<?php
+    // Your PHP code here
+    echo "Hello, World!";
+?>
+
+</body>
+</html>
